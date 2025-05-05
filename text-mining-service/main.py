5e8a98333f7d8ab4b1fe9decffedf9425c431638
Cristian Gamboa <gbcristian35@gmail.com>
2025-05-05 15:36:56 -0500

    ♻️ Refactor(main): Simplify main.py by removing production check and streamlining handler initialization

--- text-mining-service/main.py
+++ text-mining-service/main.py
@@ -1,18 +1,7 @@
-import os
+from mangum import Mangum
 from dotenv import load_dotenv
-
 load_dotenv()
 
-is_prod = os.getenv('IS_PROD', 'false').lower() == 'true'
+from app.mcp.client import app
 
-if is_prod:
-    from app.mcp.client import app
-    from mangum import Mangum
-
-    handler = Mangum(app)
-else:
-    import uvicorn
-
-    if __name__ == "__main__":
-        uvicorn.run("app.mcp.client:app", host="0.0.0.0",
-                    port=8000, reload=True)
+handler = Mangum(app)
