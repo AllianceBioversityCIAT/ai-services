a6e0715e9ed9447cebb02117d0b39a6b953e4e38
Cristian Gamboa <gbcristian35@gmail.com>
2025-11-05 10:56:17 -0500

    ✨ Load environment variables from .env file in config_util.py

--- qa-service/app/utils/config/config_util.py
+++ qa-service/app/utils/config/config_util.py
@@ -4,7 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 AWS = {
-    "aws_access_key": os.getenv("AWS_ACCESS_KEY_ID"),
-    "aws_secret_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
     "aws_region": os.getenv("AWS_REGION", "us-east-1")
 }
